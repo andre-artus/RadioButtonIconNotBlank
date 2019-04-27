cdd4cf7515d69520af6320a7c8092b88e38a1ba2
Andre Artus <andre.artus@gmail.com>
2019-04-27 10:44:52 +0200

    Icons Ignored by 1.1.0-alpha on API17

--- app/src/main/java/com/example/radiobuttoniconnotblank/MainActivity.kt
+++ app/src/main/java/com/example/radiobuttoniconnotblank/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.radiobuttoniconnotblank
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toggle_buttons.button1.buttonDrawable = null
     }
 }
